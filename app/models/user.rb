42e3c4c4d7bb120afbb5fedd46736e4c7612bb71
LittleStupid <lttlstpd@live.com>
2016-07-05 12:11:57 +0800

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,7 +11,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  attr_accessor :remember_token
+
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -28,9 +31,28 @@ class User < ActiveRecord::Base
     update_attribute( :remember_digest, nil )
   end
 
-  def authenticated?( remember_token )
-    return false if remember_digest.nil?
-    BCrypt::Password.new( remember_digest ).is_password?( remember_token )
+  def authenticated?( attribute, token )
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new( digest ).is_password?( token )
+  end
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def activate
+    update_attribute( :activated, true )
+    update_attribute( :activated_at, Time.zone.now )
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,15 @@ User.create!( name: "fuyuxiang",
               email: "fuyuxiang@live.com",
               password: "fuyuxiang",
               password_confirmation: "fuyuxiang",
-              admin: true )
+              admin: true,
+              activated: true,
+              activated_at: Time.zone.now )
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!( name: name, email: email, password: password, password_confirmation: password )
+  User.create!( name: name, email: email,
+                password: password, password_confirmation: password,
+                activated: true, activated: Time.zone.now )
 end
